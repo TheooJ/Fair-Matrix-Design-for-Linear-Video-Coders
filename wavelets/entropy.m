cb28ddb73691d73d502598da63c9d8ac688e76d7
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2022-01-31 16:21:45 +0100

    Delete moved files

--- wavelets/entropy.m
+++
@@ -1,8 +0,0 @@
-function y = entropy(x)
-% The name of this m-file is entropy.m
-% x is a data vector
-% y=entropy(x) is the first-order entropy of x
-
-P=frequency(x)/length(x);
-y=sum(-P.*log2(P));
-
